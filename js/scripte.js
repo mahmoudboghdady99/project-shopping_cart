29addacbd23815dfb04461f550642ade07c032e8
mahmoudboghdady99 <mahmoud995boghdady@gmail.com>
2022-12-26 16:18:21 +0200

    ADD new to complite project

--- js/scripte.js
+++ js/scripte.js
@@ -5,6 +5,10 @@ let userInfo=document.querySelector("#user_info")
 let userDom=document.querySelector("#user_name")
 let logOut=document.querySelector("#logout")
 
+
+
+
+
 let nameUser=localStorage.getItem("username");
 
 
@@ -19,3 +23,109 @@ logOut.addEventListener("click", function()
         window.location="register.html"
     }, 1500)
 })
+
+// products
+
+//hold variables
+let productDom=document.querySelector(".products")
+let productCart=document.querySelector(".cart-products div")
+let badgeDom=document.querySelector(".badge")
+
+let shoppingCart=document.querySelector(".shoping-cart")
+
+let cartproducts=document.querySelector(".cart-products")
+
+shoppingCart.addEventListener("click", openItems);
+
+function openItems()
+{
+    if(productCart.innerHTML !="")
+    {
+        if(cartproducts.style.display=="block")
+        {
+            cartproducts.style.display="none";
+        }else{
+            cartproducts.style.display="block";
+        }
+    }
+}
+
+
+let products = [
+    {id:1, title: "Glasses", size: "small", imgUrl: "images/glasses.jpg"},
+    {id:2, title: "Mobile", size: "large", imgUrl: "images/mobile.jpg"},
+    {id:3, title: "Tv", size: "x-large", imgUrl: "images/tv.jpg"},
+    {id:4, title: "Headphone", size: "large", imgUrl: "images/head_phone.jpg"},
+];
+
+
+//function && events
+ function drawData()
+ {
+    let productsUi = products.map((item) => 
+    {
+        return ` 
+        <div class="product-item">
+        <img src="${item.imgUrl}" alt="image" srcset="">
+    
+    
+    <div class="product-item-desc">
+    <h2>${item.title} </h2>
+    <p>Lorem ipsum dolor sit amet consectetur. </p>
+    <span>size: ${item.size}</span>
+    </div>
+    
+    <div class="product-item-action">
+    <input type="submit" onclick="addtoCart(${item.id})" value="Add to cart" class="btnShow">
+    <i class="fa fa-heart"></i>
+    </div>
+    </div>
+        
+        `
+    })
+    productDom.innerHTML=productsUi;
+ }
+drawData();
+
+function addtoCart(id)
+{
+    let chossenItem = products.find((item) => item.id===id);
+    productCart.innerHTML += ` <p>${chossenItem.title} </p> `
+
+    badgeDom.style.display="block";
+    let cartItems=document.querySelectorAll(".cart-products div p")
+    badgeDom.innerHTML = cartItems.length;
+}
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let chossenItem = products.find((item) => item.id === id);
+// cartProductDom.innerHTML += `<p>${chossenItem.title}</p>`
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
